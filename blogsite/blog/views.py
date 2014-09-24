6d7d61ee69262b5e5a6750d8ea7f179152bb8089
Dibya <ddas@stjosephcontent.com>
2014-09-24 17:04:19 -0400

    Created contact.html updated base.html and view.py

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,6 +1,12 @@
-from django.views import generic
+from django.core.mail import send_mail
 
-from models import Post, Contact
+from django.views import generic
+from forms import Contact
+from models import Post
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 
 
 class PostListView(generic.ListView):
@@ -11,5 +17,27 @@ class PostListView(generic.ListView):
 
 class ContactView(generic.FormView):
 
-    model = Contact
+    form_class = Contact
     template_name = 'contact.html'
+
+    def post(self, request):
+        raise Exception('Hey I am currently in the post method!')
+        # errors = []
+        # if request.method == 'POST':
+        #     if not request.POST.get('subject', ''):
+        #         errors.append('Enter a subject.')
+        #     if not request.POST.get('message', ''):
+        #         errors.append('Enter a message.')
+        #     if request.POST.get('email') and '@' not in request.POST['email']:
+        #         errors.append('Enter a valid e-mail address.')
+        #     if not errors:
+        #         send_mail(
+        #             request.POST['subject'],
+        #             request.POST['message'],
+        #         )
+        # return render(request, 'contact.html',
+        #     {'errors': errors})
+
+
+    def get(self, request):
+        raise Exception('Hey I am currently in the get method!')
--- blogsite/urls.py
+++ blogsite/urls.py
@@ -8,6 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^$',      PostListView.as_view(), name='PostList'),
-    url(r'^contact', ContactView.as_view(), name='Contact'),
+    url(r'^contact/', ContactView.as_view(), name='Contact'),
     url(r'^admin/', include(admin.site.urls)),
 )

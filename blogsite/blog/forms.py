17b4d6489da7b9ed5363fa90c5a8d706ef017e80
Dibya <ddas@stjosephcontent.com>
2014-09-25 16:59:11 -0400

    Added Bootstrap css code to html files.

--- blogsite/blog/forms.py
+++ blogsite/blog/forms.py
@@ -4,6 +4,10 @@ from django.forms.widgets import Textarea
 
 class ContactForm(forms.Form):
 
-    subject = forms.CharField(label='Subject', max_length=100)
+    subject = forms.CharField(
+        label='Subject',
+        max_length=100,
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
     message = forms.CharField(widget=Textarea())
     email = forms.EmailField()
